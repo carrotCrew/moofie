88c80d15803ebdcc0836dd5980df0dd1979c3844
Tyna Kpińska <45312690+tynakpinska@users.noreply.github.com>
2019-05-10 18:51:48 +0200

    Add files via upload
    corrected axios instance

--- src/api/themoviedb.js
+++ src/api/themoviedb.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.themoviedb.org/3/'
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        api_key: '4cfa00f017abc0bfb0633f700edde6da'
+    }
 });
