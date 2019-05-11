4a2036dcbd07e8b8223a2e1af283e8a8a42c0043
Hanna Domagała <domagala.h@gmail.com>
2019-05-11 22:43:03 +0200

    Fetching data + modals merged

--- src/components/MoviesList/MovieDetails/MovieDetails.js
+++ src/components/MoviesList/MovieDetails/MovieDetails.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Popup from 'reactjs-popup'
+import React from "react";
+import Popup from "reactjs-popup";
 import "./MovieDetails.css";
 import TicketBuy from "../TicketBuy/TicketBuy";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MovieDetails = () => (
-  <Popup trigger={<button className="movie__details"> 
-      More details
-        <FontAwesomeIcon
-          icon="angle-double-right"
-          className="movie__icon"
-        /> 
-    </button>} modal>
-  
+  <Popup
+    trigger={
+      <button className="movie__details">
+        More details
+        <FontAwesomeIcon icon="angle-double-right" className="movie__icon" />
+      </button>
+    }
+    modal
+  >
     {close => (
       <div className="modal">
-        <a className="close" onClick={close}>
+        <button className="close" onClick={close}>
           &times;
-        </a>
+        </button>
         <div className="header"> Avengers: Koniec gry </div>
-        
+
         <div className="content">
-        
           <p>
             <span className="info__title">Release date:</span>
             <span className="info__date">2019-04-22</span>
@@ -39,31 +39,35 @@ const MovieDetails = () => (
             <span className="info__director">Famous person</span>
           </p>
           <p>
-          <br/>
-            Po wymazaniu połowy życia we Wszechświecie przez Thanosa, Avengersi starają się zrobić wszystko co konieczne, aby pokonać szalonego tytana.
+            <br />
+            Po wymazaniu połowy życia we Wszechświecie przez Thanosa, Avengersi
+            starają się zrobić wszystko co konieczne, aby pokonać szalonego
+            tytana.
           </p>
         </div>
         <div class="siderbar">
-        <img className="img1" src="https://ssl-gfx.filmweb.pl/po/05/42/790542/7881430.6.jpg" alt="plakat - Avengers: Koniec gry (2019)"  />
+          <img
+            className="img1"
+            src="https://ssl-gfx.filmweb.pl/po/05/42/790542/7881430.6.jpg"
+            alt="plakat - Avengers: Koniec gry (2019)"
+          />
         </div>
 
-
         <div className="actions">
-        <TicketBuy name="button1"/>
-
           <button
-            className = "button1"
+            className="button1"
             onClick={() => {
-              console.log('modal closed ')
-              close()
+              close();
             }}
           >
             Close
           </button>
+
+          <TicketBuy name="button1" />
         </div>
       </div>
     )}
   </Popup>
-)
+);
 
 export default MovieDetails;
