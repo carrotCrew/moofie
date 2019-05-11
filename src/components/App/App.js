1b60733fc7de8b99507b9db9a1962d5f6123ecbd
Hanna Domagała <domagala.h@gmail.com>
2019-05-11 15:55:43 +0200

    Blah

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 // axios
-import themoviedb from '../../api/themoviedb';
+import themoviedb from "../../api/themoviedb";
 //FontAwesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -18,31 +18,41 @@ class App extends Component {
     this.state = {
       movies: [],
       genresArr: [],
-      searchTerm: ''
+      searchTerm: ""
     };
   }
 
   componentWillMount() {
-    themoviedb.get(`movie/now_playing?api_key=4cfa00f017abc0bfb0633f700edde6da&language=en&page=1`)
+    themoviedb
+      .get(
+        `movie/now_playing?api_key=4cfa00f017abc0bfb0633f700edde6da&language=en&page=1`
+      )
       .then(resp => this.setState({ movies: resp.data.results }))
       .catch(err => console.log(err));
 
-    themoviedb.get(`genre/movie/list?api_key=4cfa00f017abc0bfb0633f700edde6da&language=en`)
+    themoviedb
+      .get(
+        `genre/movie/list?api_key=4cfa00f017abc0bfb0633f700edde6da&language=en`
+      )
       .then(resp => this.setState({ genresArr: resp.data.genres }))
       .catch(err => console.log(err));
   }
 
-  passSearchTerm = (searchTerm) => {
+  passSearchTerm = searchTerm => {
     const term = searchTerm;
-    this.setState({ searchTerm: term});
-  }
+    this.setState({ searchTerm: term });
+  };
 
   render() {
     return (
       <div className="page-wrapper">
-        <Header callbackApp={this.passSearchTerm}/>
+        <Header callbackApp={this.passSearchTerm} />
         <main className="content-wrapper">
-          <MoviesList movies={this.state.movies} genresArr={this.state.genresArr} searchTerm={this.state.searchTerm}/>
+          <MoviesList
+            movies={this.state.movies}
+            genresArr={this.state.genresArr}
+            searchTerm={this.state.searchTerm}
+          />
         </main>
       </div>
     );
